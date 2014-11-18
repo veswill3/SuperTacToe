be0174ef169761b551ff96b47003729f90481d6f
Vesper Williams <veswill3@gmail.com>
2014-11-18 01:19:27 -0600

    remove a bunch of random newlines

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,14 +1,10 @@
 var player = 1
 
 $(document).ready(function() {
-
 	// tile clicked
 	$('.bTile').click(cellClick);
-	
 	// reset clicked
 	$('#resetBoard').click(resetClick);
-	
-	
 });
 
 var resetClick = function() {
@@ -17,7 +13,6 @@ var resetClick = function() {
     $('.bTile').removeAttr('disabled'); // un-disable the buttons
     $('.bTile').text(''); // remove the text
 }
-
 
 var cellClick = function() {
     
@@ -32,7 +27,6 @@ var cellClick = function() {
         $('#'+element).html('X');
     else
         $('#'+element).html('O');
-    
     
     // Get all elements from current sub-game
     var elements = elementsFromGame(element);
@@ -101,14 +95,9 @@ var elementsFromGame = function(element) {
     return elements;
 }
 
-
-
 function evalGame(game){
-    
-    
     // Possible Winning games
     var games = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-    
     
     for (var i = 0; i < games.length; i++) {
         if (compareThree(game[games[i][0]-1], game[games[i][1]-1], game[games[i][2]-1])) 
@@ -117,11 +106,7 @@ function evalGame(game){
 }
 
 var compareThree = function(one,two,three) {
-    
     if (!one || !two || !three)
         return "";
-    
     return one == two && two == three;
-    
-    
 };
